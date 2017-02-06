33ce940c964aed87acb47cb6e159cbf57a955cae
s-rebecca-fu <rebeccafu1991@gmail.com>
2017-02-05 19:33:32 -0500

    push project

---
+++ Models/CustomerValidation/WordNum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace HtmlHelperLab.Models.CustomerValidation
+{
+    public class WordNum : ValidationAttribute
+    {
+        private readonly string _chars;
+        // [Exclude ("^%$#@!")]
+
+        public WordNum(string chars) : base("{0} exceed three words.")
+        {
+            _chars = chars;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Regex expression = new Regex(@_chars);
+            //"[A-Za-z]*[ ]{1}[A-Za-z]*[ ]{1}[A-Za-z]*");
+            if (expression.IsMatch(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+            else {
+                var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                return new ValidationResult(errorMessage);
+            }
+            
+        }
+
+    }
+}
---
+++ Models/ProductPartialModel/Product.cs
@@ -0,0 +1,40 @@
+﻿using HtmlHelperLab.Models.CustomerValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelperLab.Models.NorthWindModel
+{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product { }
+
+    public class ProductMetaData {
+
+        [HiddenInput(DisplayValue = false)]
+        public object ProductID { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [Display(Name = "Product Name")]
+        [StringLength(30, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 5)]
+        [ExcludeChar("/.,!@#$%", ErrorMessage = "Name should not contain these chars: '/.,!@#$%'.")]
+        [WordNum(@"^[a-zA-Z]*[\s][a-zA-Z]*[\s][a-zA-Z]*$", ErrorMessage = "only three words maximum")]
+        public object ProductName { get; set; }
+
+        [UIHint("_SupplierDropDownList")]
+        [Display(Name = "Supplier")]
+        public object SupplierID { get; set; }
+
+        [UIHint("_CategoryDropDownList")]
+        [Display(Name = "Category")]
+        public object CategoryID { get; set; }
+
+        [Required(ErrorMessage = "Unit price is required.")]
+        [DataType(DataType.Currency)]
+        public object UnitPrice { get; set; }
+
+       
+    }
+}
